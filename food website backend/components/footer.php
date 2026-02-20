2dae3d7a42d5f223a29785e510b1c47a09dd5300
Kushmal Arora <kushmalarora017@gmail.com>
2026-02-21 00:00:49 +0530

    Add files via upload

---
+++ food website backend/components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:foodieversehub@gmail.com">foodieversehub@gmail.com</a>
+         <a href="mailto:foodieverse@gmail.com">foodieverse@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10:00am to 11:30pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Prayagraj, india - 211011</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:7778888999">7778888999</a>
+         <a href="tel:1234567890">1234567890</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Kushmal Arora</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
